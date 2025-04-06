860089d218752a1b2f40eb9f4f640ae17d9d25fe
Ravi <ravikumarrohit@gmail.com>
2025-04-06 11:18:54 +0530

    password generator project

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -54,7 +54,7 @@ function App() {
         />
         <button
         onClick={copyPasswordToClipboard}
-        className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
+        className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0 hover:bg-blue-400 cursor-pointer'
         >copy</button>
         
     </div>
